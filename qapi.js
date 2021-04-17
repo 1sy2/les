19fbe53b7948f8d39285fd90206184bdf82ab6fd
1sy2 <82720472+1sy2@users.noreply.github.com>
2021-04-17 19:03:07 +0300

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,31 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"60772be923a143001e08e9f9",
+				"60772be923a143001e08e9fb"
+			]
+		},
+		"60772be923a143001e08e9f9": {
+			"id": "60772be923a143001e08e9f9",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"60772be923a143001e08e9fb": {
+			"id": "60772be923a143001e08e9fb",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {
+			"position": "static"
+		},
+		"seo": {}
+	}
+}
